d2b4b4dcd3cbe329f4e9105d3c6a8ba9fc7d52d4
unknown <Pastor@PCO-LAPTOP>
2014-05-04 00:03:40 -0600

    Addition for VS2013 solution

---
+++ MVC-nicadevs/MVC-nicadevs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_nicadevs.Startup))]
+namespace MVC_nicadevs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
