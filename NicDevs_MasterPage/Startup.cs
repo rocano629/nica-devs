0fd5df11250fe42b6a0c876ffc2787a6bd56713f
bcanales <byroncanales@hotmail.com>
2014-05-18 01:08:25 -0600

    Pagina Principal de Nuestro sitio verison 1.0.0.0.1

---
+++ NicDevs_MasterPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NicDevs_MasterPage.Startup))]
+namespace NicDevs_MasterPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
